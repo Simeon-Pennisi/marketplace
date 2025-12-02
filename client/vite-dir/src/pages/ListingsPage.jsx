bd2fc58dfe34b02eb3062431c908f1e0245ad403
Simeon Pennisi <simeonpennisi@gmail.com>
2025-12-02 01:04:38 -0500

    Refactor: updated ListingsPage.jsx

--- client/vite-dir/src/pages/ListingsPage.jsx
+++ client/vite-dir/src/pages/ListingsPage.jsx
@@ -1,1 +1,12 @@
-// page for item listings
+import "../styles/pages/listings.css";
+
+function ListingsPage() {
+  return (
+    <div className="page listings-page">
+      <h1>Browse Listings</h1>
+      <p>(Filters + Grid will go here in Chapter 2)</p>
+    </div>
+  );
+}
+
+export default ListingsPage;
