ef9aad2a1bd94f91961d9713e6f306e3f00829a6
Simeon Pennisi <simeonpennisi@gmail.com>
2025-12-02 01:12:26 -0500

    Refactor: updated RegisterPage.jsx

--- client/vite-dir/src/pages/RegisterPage.jsx
+++ client/vite-dir/src/pages/RegisterPage.jsx
@@ -1,1 +1,12 @@
-// create account
+import "../styles/pages/register.css";
+
+function RegisterPage() {
+  return (
+    <div className="page register-page">
+      <h1>Create an Account</h1>
+      <p>(Registration form coming in Chapter 3)</p>
+    </div>
+  );
+}
+
+export default RegisterPage;
