d73ed35ff53d7aae6d35142d8a884fc51594229e
Simeon Pennisi <simeonpennisi@gmail.com>
2025-12-02 01:03:29 -0500

    Refactor: updated HomePage.jsx added some text

--- client/vite-dir/src/pages/HomePage.jsx
+++ client/vite-dir/src/pages/HomePage.jsx
@@ -1,1 +1,12 @@
-// the home page
+import "../styles/pages/home.css";
+
+function HomePage() {
+  return (
+    <div className="page home-page">
+      <h1>Welcome to TechMarket</h1>
+      <p>Your marketplace for quality refurbished tech gear.</p>
+    </div>
+  );
+}
+
+export default HomePage;
