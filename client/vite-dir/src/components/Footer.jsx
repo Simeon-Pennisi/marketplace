8d9de579815f5cdd582cc210fb2873f96da4a7f8
Simeon Pennisi <simeonpennisi@gmail.com>
2025-12-02 01:01:42 -0500

    Refactor: updated Footer.jsx

--- client/vite-dir/src/components/Footer.jsx
+++ client/vite-dir/src/components/Footer.jsx
@@ -1,1 +1,14 @@
-// footer component
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        © {new Date().getFullYear()} TechMarket by Simeon Pennisi \n All rights
+        reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
