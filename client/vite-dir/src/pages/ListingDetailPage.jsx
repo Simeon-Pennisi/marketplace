8298300a7b5d725b8486b8143f2003c57790d24e
Simeon Pennisi <simeonpennisi@gmail.com>
2025-12-02 01:06:10 -0500

    Refactor: updated ListingsDetailPage.jsx

--- client/vite-dir/src/pages/ListingDetailPage.jsx
+++ client/vite-dir/src/pages/ListingDetailPage.jsx
@@ -1,1 +1,12 @@
-// details about listing
+import "../styles/pages/listingDetail.css";
+
+function ListingDetailPage() {
+  return (
+    <div className="page listing-detail-page">
+      <h1>Listing Detail</h1>
+      <p>(Full listing info will go here)</p>
+    </div>
+  );
+}
+
+export default ListingDetailPage;
