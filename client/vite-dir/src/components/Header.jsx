7415ce2a5f25dc26c1ad6366e6a15f732f35a8fe
Simeon Pennisi <simeonpennisi@gmail.com>
2025-12-02 00:58:35 -0500

    Refactor: updated Header.jsx added links to pages

--- client/vite-dir/src/components/Header.jsx
+++ client/vite-dir/src/components/Header.jsx
@@ -1,1 +1,23 @@
-// Header component
+import { Link } from "react-router-dom";
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <Link to="/" className="logo">
+          TechMarket
+        </Link>
+
+        <nav className="nav">
+          <Link to="/listings">Browse</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+          <Link to="/dashboard">Dashboard</Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
