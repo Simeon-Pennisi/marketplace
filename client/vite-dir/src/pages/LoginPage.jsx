d39913b91cfcde9b7d73e1f7dc24d39f53e9d7f1
Simeon Pennisi <simeonpennisi@gmail.com>
2025-12-02 01:07:01 -0500

    Refactor: updated LoginPage.jsx

--- client/vite-dir/src/pages/LoginPage.jsx
+++ client/vite-dir/src/pages/LoginPage.jsx
@@ -1,1 +1,12 @@
-// profile login
+import "../styles/pages/login.css";
+
+function LoginPage() {
+  return (
+    <div className="page login-page">
+      <h1>Login</h1>
+      <p>(Auth form coming in Chapter 3)</p>
+    </div>
+  );
+}
+
+export default LoginPage;
