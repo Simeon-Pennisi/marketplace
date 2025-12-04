54f5a1a0f9c01d8613d33ef327f037978b8ef507
Simeon Pennisi <simeonpennisi@gmail.com>
2025-12-03 21:03:59 -0500

    Updated server/src/db/index.js

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,1 +1,21 @@
 // pg pool
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  console.error("❌ DATABASE_URL is not set in .env");
+  process.exit(1);
+}
+
+const pool = new Pool({
+  connectionString,
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle Postgres client", err);
+  process.exit(-1);
+});
+
+module.exports = pool;
