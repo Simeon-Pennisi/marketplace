718ed86e5405f4eb14f67c6a63ad58b2bf0b5217
Simeon Pennisi <simeonpennisi@gmail.com>
2026-01-16 04:49:36 -0500

    updated main.jsx to include AuthProvider wrapper from AuthContext.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,39 +1,16 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "./index.css";
-// import App from "./App.jsx";
-
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
-//
-// Here is code from ChatGPT
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-//
-// Here is the code from the T10 Hello-Vite project
-// import { StrictMode } from "react";
-// import { ReactDOM } from "react-dom/client"; // never used
-// import { createRoot } from "react-dom/client";
-// import App from "./components/App.jsx";
-// import "./index.css";
-
-// // createRoot(document.getElementById("root")).render(
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
