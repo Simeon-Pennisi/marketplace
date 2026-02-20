d08fb923c7ebcea67f77fc5e75786a31228fa5a7
Simeon Pennisi <simeonpennisi@gmail.com>
2026-02-20 11:23:37 -0500

    updated dotenv to be conditional

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,18 +1,21 @@
 import { Pool } from "pg";
-import "dotenv/config";
 
 const isProd = process.env.NODE_ENV === "production";
+
+// Only loading .env locally
+if (!isProd) {
+  await import("dotenv/config");
+}
 
 const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
   console.error("❌ DATABASE_URL is not set. Check your .env file.");
-  process.exit(1);
+  throw new Error("DATABASE_URL is not set");
 }
-// const pool = new Pool({ connectionString });
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString,
   ssl: isProd ? { rejectUnauthorized: false } : false,
 });
 
