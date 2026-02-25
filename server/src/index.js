2e36deb7102186c26b4ee5e24d64bdfef4458c9b
Simeon Pennisi <simeonpennisi@gmail.com>
2026-02-25 00:36:16 -0500

    wrapping dotenv/config to only run locally

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,10 @@
 import "dotenv/config";
 import app from "./app.js";
 
+if (process.env.NODE_ENV !== "production") {
+  await import("dotenv/config");
+}
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
